395f582f059a38088dffa6ce1ee69a903829109a
Rahul Barman <47694000+rahulbarman03@users.noreply.github.com>
2022-10-01 18:46:47 +0530

    Create left rotate an array k times.cpp

---
+++ left rotate an array k times.cpp
@@ -0,0 +1,33 @@
+//left rotate an array K number of times
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRotate(int arr[], int n, int k)
+{
+	int mod = k % n;
+  
+	for (int i = 0; i < n; i++)
+		cout << (arr[(mod + i) % n]) << " ";
+
+	cout << "\n";
+}
+
+int main()
+{
+  int n;
+  int arr[100];
+  cin>>n;
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+	int k = 2;
+	leftRotate(arr, n, k);
+
+	k = 3;
+	leftRotate(arr, n, k);
+
+	k = 4;
+	leftRotate(arr, n, k);
+
+	return 0;
+}
